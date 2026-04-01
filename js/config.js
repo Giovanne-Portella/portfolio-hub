0fd6215a658e129b7a2323ef6172ef707c2f54f0
Giovanne Marinho <giovannepb@hotmail.com>
2026-04-01 17:34:04 -0300

    fix: resolve supabase global identifier conflict with CDN

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
 const SUPABASE_URL = 'https://wejrqqqblubxlwmztzmt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlanJxcXFibHVieGx3bXp0em10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzIzMTgsImV4cCI6MjA5MDY0ODMxOH0.Ylfto8T9au3Q3P_0Qs_NMX1TRDgjB6IRYmEDS1qKzo4';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
